7f06000e285e1fef0d4e48e4bdae8050cbe589a5
udaysarkarud <udayssarkarud@gmail.com>
2023-07-06 19:05:24 +0600

    [DONE] prettier check

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
   try {
-    await mongoose.connect(config.databse_url as string)
-    console.log('db connected')
+    await mongoose.connect(config.databse_url as string);
+    console.log('db connected');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
+      console.log(`Example app listening on port ${config.port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-main()
+main();
