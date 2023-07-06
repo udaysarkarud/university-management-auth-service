3da5f8d83e93d344217817dfdf9d949d87ba4192
udaysarkarud <udayssarkarud@gmail.com>
2023-07-06 18:49:57 +0600

    [DONE] eslint setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,6 @@ import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
-interface Config {
-  NODE_ENV: string;
-  PORT: number;
-  MONGO_URI: string;
-}
-
 export default {
   port: process.env.PORT,
   databse_url: process.env.DATABASE_URL,
