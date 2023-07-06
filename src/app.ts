f935808f204f4ef8dbb0d65654cb7a939da96062
udaysarkarud <udayssarkarud@gmail.com>
2023-07-06 18:29:39 +0600

    [DONE] setup perser cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express, { Application } from "express";
+import cors from "cors";
+
 const app: Application = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
