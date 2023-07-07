4a11b8ec422d0988fd66ac5df430dce1283097f5
udaysarkarud <udayssarkarud@gmail.com>
2023-07-07 19:10:34 +0600

    [DONE] winston setup and confidure with winston rotation

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import logger from './shared/logger';
 
 async function main() {
   try {
     await mongoose.connect(config.databse_url as string);
-    console.log('db connected');
+    logger.loggerInfo.info('db connected');
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      logger.loggerInfo.info(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log(error);
+    logger.loggerError.error(error);
   }
 }
 
---
+++ src/shared/logger.ts
@@ -0,0 +1,60 @@
+import { createLogger, format, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import path from 'path';
+const { combine, timestamp, label, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp);
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  return `${date.toDateString()} | ${hour}:${minutes} [${label}] ${level}: ${message}`;
+});
+
+const loggerInfo = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'umauth' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu--%DATE%-success.log',
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+const loggerError = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'umauth' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'error',
+        'phu--%DATE%-error.log',
+      ),
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+});
+
+export default {
+  loggerInfo,
+  loggerError,
+};
+
+/* 12-7 */
